f8adcf6f7787be47ecb83eba979d34feefaa8b96
Carl Smith <carlfsmithiii@gmail.com>
2018-11-15 14:50:34 -0500

    Change 'class' to 'className' in src/components/footer.js jsx

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,10 +3,10 @@ import React from "react";
 export default function Footer(props) {
   return (
     <footer className="footer">
-      <span class="todo-count">
+      <span className="todo-count">
         <strong>{props.todo_count}</strong> item(s) left
       </span>
-      {props.todo_count > 0 && <button class="clear-completed">Clear completed</button>}
+      {props.todo_count > 0 && <button className="clear-completed">Clear completed</button>}
     </footer>
   );
 }
