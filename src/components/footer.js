97f013d9dad6119bda8ee912986d90a6b960796a
Carl Smith <carlfsmithiii@gmail.com>
2018-11-20 09:54:02 -0500

    remove import of constants for ALL, ACTIVE, and COMPLETED

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { NavLink } from 'react-router-dom';
 
-import { ALL, ACTIVE, COMPLETED } from './App';
-
 export default function Footer(props) {
   return (
     <footer className="footer">
