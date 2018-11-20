153f970486371804afd49e375b9c3c9c3dbe357c
Carl Smith <carlfsmithiii@gmail.com>
2018-11-19 20:40:03 -0500

    Wrapped <App /> in <BrowserRouter>

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './components/app';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Index = () => (
+    <BrowserRouter>
+        <App /> 
+    </BrowserRouter>
+);
+
+ReactDOM.render(<Index />, document.getElementById('root'));
