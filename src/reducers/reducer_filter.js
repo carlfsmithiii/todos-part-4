c0efcedd0c19c4f0d8d90015fe2b72a49a5574a1
Carl Smith <carlfsmithiii@gmail.com>
2018-11-28 10:12:33 -0500

    remove dead code

--- src/reducers/reducer_filter.js
+++ src/reducers/reducer_filter.js
@@ -4,10 +4,6 @@ import { initialState } from '../components/TodoList';
 export default function(state = initialState.filter , action) {
     switch(action.type) {
         case SET_FILTER:
-            // return {
-            //     // ...state,
-            //     filter: action.payload
-            // };
             return action.payload;
         default:
             return state;
